01f64ec1ad77e666562c76c9cfc7e1310fc48cfe
LailaKhatibi <91633860+LailaKhatibi@users.noreply.github.com>
2021-11-08 19:45:08 -0500

    Version_Laila

---
+++ GeoExplor1/app/src/main/java/com/example/geoexplor1/Enregistrer.kt
@@ -0,0 +1,39 @@
+package com.example.geoexplor1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.firebase.database.FirebaseDatabase
+import java.util.jar.Attributes
+
+
+class Enregistrer : AppCompatActivity() {
+    var database = FirebaseDatabase.getInstance()
+    var name = database.getReference("message")
+    var email =
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enregistrer)
+        m_name = (EditText) findViewById(R.id.Name);
+
+
+    }
+
+    fun Continue(view: View?) {
+       // myRef.setValue();
+
+
+
+
+
+    }
+
+
+    fun Retour(view: View?) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+}
