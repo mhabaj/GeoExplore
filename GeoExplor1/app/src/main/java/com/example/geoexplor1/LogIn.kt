45948952c98afbd3e5807e61ce9a50b586ee6be6
LailaKhatibi <91633860+LailaKhatibi@users.noreply.github.com>
2021-11-11 17:50:20 -0500

    auth_v4

--- GeoExplor1/app/src/main/java/com/example/geoexplor1/LogIn.kt
+++ GeoExplor1/app/src/main/java/com/example/geoexplor1/LogIn.kt
@@ -3,12 +3,32 @@ package com.example.geoexplor1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
+
+    private var m_email: EditText? = null
+    private var m_password: EditText? = null
+    private val TAG = "MyActivity"
+    private var progress_bar : ProgressBar? =null
+    private var m_Resultat: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+
+        m_email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        m_password = findViewById<EditText>(R.id.editTextTextPassword)
+        progress_bar = findViewById<ProgressBar>(R.id.progressBar)
+        m_Resultat = findViewById<TextView>(R.id.textAffichageResultat)
+
     }
 
 
@@ -19,8 +39,49 @@ class LogIn : AppCompatActivity() {
     }
 
     fun PageAccueil(view: View?) {
-        val intent = Intent(this, Accueil::class.java)
-        startActivity(intent)
+
+
+        var f_auth = FirebaseAuth.getInstance()
+/*
+        if(f_auth.currentUser != null) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }*/
+
+
+        //Verification
+        var email = m_email?.text.toString().trim()
+        var password = m_password?.text.toString().trim()
+
+        if(TextUtils.isEmpty(email)){
+            m_email?.setError("Email is Required.")
+            return
+        }
+        if(TextUtils.isEmpty(password)){
+            m_password?.setError("Password is Required.")
+            return
+        }
+        if(m_password?.length()!! <6 ){
+            m_password?.setError("Password must have at least 6 characters.")
+            return
+        }
+
+        progress_bar?.setVisibility(View.VISIBLE)
+        m_Resultat?.setVisibility(View.VISIBLE)
+
+        // Enregistrer l'utilisateur dans la base de données
+
+        f_auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { taskId ->
+            if(taskId.isSuccessful) {
+                m_Resultat?.setText("Logged in Succesfully ! ")
+                startActivity(Intent(applicationContext, Accueil::class.java))
+            }
+            else {
+                m_Resultat?.setText("Error !!"+ taskId.exception)
+                progress_bar?.setVisibility(View.INVISIBLE)
+            }
+        }
+
 
     }
 
