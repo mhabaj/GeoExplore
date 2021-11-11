f45ab7c44d9835fd0cea745733317434a0435a68
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2021-11-11 09:38:02 -0500

    Ajout de script pour générer et lire un qrcode

--- GeoExplorerDesign/app/src/main/java/uqac/geoexplorer/MainActivity.kt
+++ GeoExplorerDesign/app/src/main/java/uqac/geoexplorer/MainActivity.kt
@@ -1,11 +1,24 @@
 package uqac.geoexplorer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.friends)
+    }
+    fun GenerateQrCode(view: View?) {
+        val intent = Intent(this, QrCodeActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    fun ScanQrCode(view: View?) {
+        val intent = Intent(this, ReadQrCode::class.java)
+        startActivity(intent)
+
     }
 }
