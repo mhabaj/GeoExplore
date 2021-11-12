dc2f180afc4de66f0d6735e6d304180f72a6f94c
LailaKhatibi <91633860+LailaKhatibi@users.noreply.github.com>
2021-11-11 19:33:35 -0500

    Auth_vFinal (logIn, SignUp, LogOut)

--- GeoExplor1/app/src/main/java/com/example/geoexplor1/Settings.kt
+++ GeoExplor1/app/src/main/java/com/example/geoexplor1/Settings.kt
@@ -4,23 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
-class Accueil : AppCompatActivity() {
+class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_accueil)
+        setContentView(R.layout.activity_settings)
     }
 
-    fun RechercherCourse(view: View){
-        val intent = Intent(this, Recherche::class.java)
+    fun LogOut(view: View?) {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
+        finish()
     }
 
-    fun Settings(view: View?) {
-        val intent = Intent(this, Settings::class.java)
-        startActivity(intent)
 
-    }
+
 
 
 }
