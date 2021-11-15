739697542b0548f8afd01a6cf695bacaed9f4af6
LailaKhatibi <91633860+LailaKhatibi@users.noreply.github.com>
2021-11-15 18:36:00 -0500

    reset mdp+ settings + profile

--- app/src/main/java/com/uqac/geoexplore/activity/Accueil.kt
+++ app/src/main/java/com/uqac/geoexplore/activity/Accueil.kt
@@ -20,8 +20,14 @@ class Accueil : AppCompatActivity() {
         val intent = Intent(this, Recherche::class.java)
         startActivity(intent)
     }
-    fun Settings(view: View?) {
-        val intent = Intent(this, Settings::class.java)
+    fun settings(view: View?) {
+        val intent = Intent(this, Settings2::class.java)
+        startActivity(intent)
+
+    }
+
+    fun profile(view: View?) {
+        val intent = Intent(this, Profile::class.java)
         startActivity(intent)
 
     }
--- app/src/main/java/com/uqac/geoexplore/activity/Settings2.kt
+++ app/src/main/java/com/uqac/geoexplore/activity/Settings2.kt
@@ -4,30 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.uqac.geoexplore.MainActivity
 import com.uqac.geoexplore.R
 
-
-class Profile() : AppCompatActivity() {
-
-
+class Settings2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-
-        val user = FirebaseAuth.getInstance().currentUser
-        var m_name = findViewById<TextView>(R.id.textname)
-
-        m_name.text = user?.displayName
-
+        setContentView(R.layout.activity_settings2)
     }
 
-
-    fun settings(view: View?) {
-        val intent = Intent(this, Settings2::class.java)
+    fun LogOut(view: View?) {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
-
+        finish()
     }
 }
-
