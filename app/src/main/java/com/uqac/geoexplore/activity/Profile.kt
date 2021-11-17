ea70b1cc4a24a0d1bee6836409ed760e618473c9
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2021-11-16 20:16:41 -0500

    image s'enregistre dans firebase

--- app/src/main/java/com/uqac/geoexplore/activity/Profile.kt
+++ app/src/main/java/com/uqac/geoexplore/activity/Profile.kt
@@ -1,23 +1,120 @@
 package com.uqac.geoexplore.activity
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.TextView
-import com.google.firebase.auth.FirebaseAuth
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide.with
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import com.uqac.geoexplore.Functions
 import com.uqac.geoexplore.R
+import com.uqac.geoexplore.model.User
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.profil_friends.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.util.*
 
 class Profile : AppCompatActivity() {
+    var user: User? = null
+    var dbUser: FirebaseUser? = null
+    var image:String? = null
+    var db: FirebaseFirestore? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-
         var m_name = findViewById<TextView>(R.id.textname)
-        val user = FirebaseAuth.getInstance().currentUser
-        m_name!!.text = user?.displayName
 
+        db = Firebase.firestore
+       dbUser = Firebase.auth.currentUser
+
+        m_name!!.text = dbUser?.displayName
+
+        selectphoto_button_register.setOnClickListener {
+            Log.d(TAG, "Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+        MainScope().launch {
+            user = Functions.getUserFromUid(dbUser!!.uid)
+            Log.d("App", user.toString())
+            if (user!!.profileImageUrl != null) {
+                var storageReference = FirebaseStorage.getInstance().getReference().child("myimage");
+
+                var myUri = Uri.parse(user!!.profileImageUrl!!)
+                //val ref = FirebaseStorage.getInstance().getReference("/images/").child(myUri.path!!).downloadUrl.
+                Glide.with(this@Profile).load(myUri).into(selectphoto_imageview_register)
+
+                //Picasso.get().load(ref).into(selectphoto_imageview_register)
+                selectphoto_button_register.alpha = 0f
+            }
+        }
+
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            // proceed and check what the selected image was....
+            Log.d(TAG, "Photo was selected")
+
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            selectphoto_imageview_register.setImageBitmap(bitmap)
+
+            selectphoto_button_register.alpha = 0f
+
+            val filename = UUID.randomUUID().toString()
+            val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+            val uploadTask = ref.putFile(selectedPhotoUri!!)
+            uploadTask.continueWith {
+                if (!it.isSuccessful) {
+                    it.exception?.let { t ->
+                        throw t
+                    }
+                }
+                ref.downloadUrl
+            }.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    it.result!!.addOnSuccessListener { task ->
+                        image = task.toString()
+                        MainScope().launch {
+                            user = Functions.getUserFromUid(dbUser!!.uid)
+                            user!!.profileImageUrl = image
+                            Log.d("App", image!!.toString())
+                            db!!.collection("User").document(dbUser!!.uid.toString()).set(user!!)
+
+                            Log.d("App", user.toString())
+                        }
+                    }
+                }
+            }
+
+
+        }
     }
 
     fun settings(view: View?) {
--- app/src/main/java/com/uqac/geoexplore/model/User.kt
+++ app/src/main/java/com/uqac/geoexplore/model/User.kt
@@ -1,12 +1,13 @@
 package com.uqac.geoexplore.model
 
+import android.net.Uri
 
 
-class User(var id: String?=null, var shownName: String?=null, var email: String?=null, var friends: List<String>?= null) {
+class User(var id: String?=null, var shownName: String?=null, var email: String?=null, var profileImageUrl: String? = null, var friends: List<String>?= null) {
 
 
     override fun toString(): String {
-        return "User(id=$id, shownName=$shownName, email=$email, friends=$friends)"
+        return "User(id=$id, shownName=$shownName, email=$email,profileImageUrl=$profileImageUrl, friends=$friends)"
     }
 
 }
