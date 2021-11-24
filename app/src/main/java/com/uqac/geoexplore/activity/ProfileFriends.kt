fc06720f563d094ed77b31d4c811d2705317ea10
IlonaPw <74210552+IlonaPw@users.noreply.github.com>
2021-11-24 15:24:51 -0500

    photo profil amie

--- app/src/main/java/com/uqac/geoexplore/activity/ProfileFriends.kt
+++ app/src/main/java/com/uqac/geoexplore/activity/ProfileFriends.kt
@@ -1,7 +1,9 @@
 package com.uqac.geoexplore.activity
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +11,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import com.uqac.geoexplore.Functions
 import com.uqac.geoexplore.R
 import com.uqac.geoexplore.model.User
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.profil_friends.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
@@ -31,6 +36,13 @@ class ProfileFriends : AppCompatActivity() {
         MainScope().launch {
             user = Functions.getUserFromUid(message!!)
             Name!!.text = user?.shownName
+            if (user!!.profileImageUrl != null) {
+
+                    var myUri = Uri.parse(user!!.profileImageUrl!!)
+
+                    Picasso.get().load(myUri).into(imageView)
+            }
+
         }
 
 
