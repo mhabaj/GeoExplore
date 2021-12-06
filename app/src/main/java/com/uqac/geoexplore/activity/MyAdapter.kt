30d5ecfcf4d6236bdabc0d7a0c94cdca07c23009
LailaKhatibi <91633860+LailaKhatibi@users.noreply.github.com>
2021-12-06 01:34:45 -0500

    recycle view

---
+++ app/src/main/java/com/uqac/geoexplore/activity/MyAdapter.kt
@@ -0,0 +1,41 @@
+package com.uqac.geoexplore.activity
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.uqac.geoexplore.R
+import com.uqac.geoexplore.model.Course
+
+class MyAdapter( private val courseList : ArrayList<Course>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView= LayoutInflater.from(parent.context).inflate(R.layout.course_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentitem = courseList[position]
+        holder.name.text = currentitem.name
+        holder.description.text = currentitem.miscInfo?.description
+        holder.difficulty.text = currentitem.miscInfo?.difficulty.toString()
+        holder.rating.text = currentitem.miscInfo?.rating.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return courseList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val name : TextView= itemView.findViewById(R.id.tvName)
+        val difficulty : TextView= itemView.findViewById(R.id.tvDifficulty)
+        val description : TextView= itemView.findViewById(R.id.tvdescription)
+        val rating : TextView= itemView.findViewById(R.id.tvrating)
+
+    }
+
+}
--- app/src/main/java/com/uqac/geoexplore/activity/Recherche.kt
+++ app/src/main/java/com/uqac/geoexplore/activity/Recherche.kt
@@ -1,12 +1,59 @@
 package com.uqac.geoexplore.activity
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import com.uqac.geoexplore.R
+import com.uqac.geoexplore.model.Course
+import kotlinx.android.synthetic.main.activity_recherche.*
+import kotlinx.android.synthetic.main.create_course.*
+
+
+
 
 class Recherche : AppCompatActivity() {
+    private lateinit var db: FirebaseFirestore
+    private lateinit var courseRecycleview : RecyclerView
+    private lateinit var courseArraylist : ArrayList<Course>
+    private lateinit var my_adapter :MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recherche)
+
+        courseRecycleview = findViewById(R.id.courseList)
+        courseRecycleview.layoutManager= LinearLayoutManager(this)
+        courseRecycleview.setHasFixedSize(true)
+
+        db = FirebaseFirestore.getInstance()
+        db.collection("Course")
+
+
+
+
+
     }
+
+
+
+    private fun getCourseData(){
+
+        db = Firebase.firestore
+        db.collection("Course")
+
+
+
+
+    }
+
+
+
 }
